8723d6e669637379c42b5524a4ce83865507db92
Narek Hovhannisyan <hnarek2005@gmail.com>
2024-09-08 19:16:13 +0400

    🔍️ Added basic SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import React from "react";
+
+const Page = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen dark:bg-black dark:text-white pt-24">
+      <div className="container max-w-3xl mx-auto py-10">
+        <h1 className="text-4xl font-bold mb-4">Welcome to the Dark Page</h1>
+        <p className="text-lg">
+          This is a basic page with a black background and white text.
+        </p>
+        <p className="mt-4">
+          Feel free to add more content here. The header and footer have their
+          own background styling, so they won't be affected by this page's
+          background.
+        </p>
+      </div>
     </div>
   );
-}
+};
+
+export default Page;
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,19 @@
 type TSiteConfig = {
+  url: string
   profilePictureUrl: string;
+  socialMedia: {
+    linkedIn: string
+    telegram: string
+    github: string
+  }
 };
 
 export const siteConfig: TSiteConfig = {
-  profilePictureUrl:
-    "https://media.licdn.com/dms/image/v2/D4E03AQHtNCb48nW-5g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1693147221019?e=1730332800&v=beta&t=OnWBAnG9W1zPYSjOyBr-cTtGyLssZACNo_s7nfswi7k",
+  url: "http://localhost:3000",
+  profilePictureUrl: "https://media.licdn.com/dms/image/v2/D4E03AQHtNCb48nW-5g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1693147221019?e=1730332800&v=beta&t=OnWBAnG9W1zPYSjOyBr-cTtGyLssZACNo_s7nfswi7k",
+  socialMedia: {
+    linkedIn: "https://www.linkedin.com/in/narek-hovhannisyan-241544255/",
+    telegram: "",
+    github: ""
+  }
 };
