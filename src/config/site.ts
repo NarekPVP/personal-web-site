a08678a1a40a0937bc2cf0b42e9d0f8bc80a4659
Narek Hovhannisyan <57366386+NarekPVP@users.noreply.github.com>
2025-01-19 20:32:54 +0400

    🔧 Updated site configs

--- src/config/site.ts
+++ src/config/site.ts
@@ -17,9 +17,9 @@ type TSiteConfig = {
 };
 
 export const siteConfig: TSiteConfig = {
-  url: "http://localhost:3000",
+  url: "https://narekhovhannisyan.com",
   githubRepoUrl: "https://github.com/NarekPVP/personal-web-site",
-  profilePictureUrl: "https://media.licdn.com/dms/image/v2/D4E03AQHtNCb48nW-5g/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1693147221019?e=1736380800&v=beta&t=oE_rBsVDj9RsVA8WSrNv8IKOnW-EHhpSgY0tZSf2TDA",
+  profilePictureUrl: "https://media.licdn.com/dms/image/v2/D4E03AQHtNCb48nW-5g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1693147221019?e=1743033600&v=beta&t=ARXMlASfoRKyO1SR43m3Kp3EH3nfsLjQnNVPu33d-go",
   username: "NarekDev",
   email: "hnarek2005@gmail.com",
   phone: "+374 99 691 641",
