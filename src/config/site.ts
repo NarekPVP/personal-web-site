3a7bfdb83e4c67a03fd7d3f86fb74687970c0b52
Narek Hovhannisyan <57366386+NarekPVP@users.noreply.github.com>
2025-04-25 16:37:57 +0400

    🐛 Fixed profile picture not showing

--- src/config/site.ts
+++ src/config/site.ts
@@ -19,7 +19,7 @@ type TSiteConfig = {
 export const siteConfig: TSiteConfig = {
   url: "https://narekhovhannisyan.com",
   githubRepoUrl: "https://github.com/NarekPVP/personal-web-site",
-  profilePictureUrl: "https://media.licdn.com/dms/image/v2/D4E03AQHtNCb48nW-5g/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1693147221019?e=1743033600&v=beta&t=ARXMlASfoRKyO1SR43m3Kp3EH3nfsLjQnNVPu33d-go",
+  profilePictureUrl: "https://media.licdn.com/dms/image/v2/D4E03AQHtNCb48nW-5g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1693147221019?e=1750896000&v=beta&t=KuACXZE72WLl6cByvxup8eq7BgdBgeRdmHfHosX6t6A",
   username: "NarekDev",
   email: "hnarek2005@gmail.com",
   phone: "+374 99 691 641",
