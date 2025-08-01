bbf36c077df362f59de63de06a6c044b27dcbbe6
Narek Hovhannisyan <hnarek2005@gmail.com>
2025-08-01 19:59:50 +0400

    fix: fixed not showing avatar

--- src/config/site.ts
+++ src/config/site.ts
@@ -19,7 +19,7 @@ type TSiteConfig = {
 export const siteConfig: TSiteConfig = {
   url: "https://narekhovhannisyan.com",
   githubRepoUrl: "https://github.com/NarekPVP/personal-web-site",
-  profilePictureUrl: "https://media.licdn.com/dms/image/v2/D4E03AQHtNCb48nW-5g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1693147221019?e=1750896000&v=beta&t=KuACXZE72WLl6cByvxup8eq7BgdBgeRdmHfHosX6t6A",
+  profilePictureUrl: "/avatar.jpeg",
   username: "NarekDev",
   email: "hnarek2005@gmail.com",
   phone: "+374 99 691 641",
